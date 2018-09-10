a771a3322777f7acbecbca82a96163d1306cf4eb
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-09-10 13:00:14 -0400

    Add the template version of the ExploreDataTypes file.

---
+++ ExploreDataTypes.java
@@ -0,0 +1,34 @@
+package inclass;
+
+import java.util.Date;
+
+/** This class explores different data types.
+ *  The first lines of code will declare variables. Then, the program will
+ *  try to assign a value to a variable that is declared as a constant.
+ *
+ * <p>Bugs: This program has no known bugs.
+ *
+ * @author Gregory M. Kapfhammer
+ */
+
+public class ExploreDataTypes {
+
+  /** The main entry point for ExploreDataTypes.
+   *  The source code inside of this method will be executed when the program is
+   *  run in the command-line. The output of this program will appear in the
+   *  terminal window. The println statements make the program display output
+   *  about the creation and use of different data types.
+   *
+   * @param args The command-line arguments
+   **/
+  public static void main(String[] args) {
+    // declare variables of different data types
+    int age = 19;
+    double pi = 3.14159265358979;
+    float weight = 122.5f;
+    String name = "Gregory M. Kapfhammer";
+    String space = " ";
+    // print out all of the variables
+  }
+
+}
