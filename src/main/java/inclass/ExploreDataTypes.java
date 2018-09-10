7f0c3e790a4d3b3ebb2062a6c2a7aa8e15706d1d
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-09-10 13:00:36 -0400

    Add the first version of the ExploreDataTypes.

---
+++ src/main/java/inclass/ExploreDataTypes.java
@@ -0,0 +1,31 @@
+package inclass;
+
+import java.util.Date;
+
+/** This class displays four lines of output.
+ * The first line displays the name of the programmer and the date. Each one of
+ * the next three lines displays a single statement created by the programmer.
+ *
+ * <p>Bugs: This program has no known bugs.
+ *
+ * @author Gregory M. Kapfhammer
+ */
+
+public class ExploreDataTypes {
+
+  /** The main entry point for ExploreDataTypes.
+   *  The source code inside of this method will be executed when the program is
+   *  run in the command-line. The output of this program will appear in the
+   *  terminal window. The println statements make the program display output
+   *  about the creation and use of different data types.
+   *
+   * @param args The command-line arguments
+   **/
+  public static void main(String[] args) {
+    // declare variables of different data types
+    int age = 19;
+    String name = "Gregory M. Kapfhammer";
+
+  }
+
+}
